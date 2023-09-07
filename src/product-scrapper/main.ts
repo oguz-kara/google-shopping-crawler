c4d59da5748dd3f64a3c685c65f09b00d8461e6a
oguz-kara <dev.hasan.kara@gmail.com>
2023-09-07 18:29:49 +0300

    push to github to continue at home

--- src/product-scrapper/main.ts
+++ src/product-scrapper/main.ts
@@ -1,27 +1,6 @@
-import puppeteer, { Page } from 'puppeteer'
-import { googleConfig } from './config'
-import { scrapeGoogleProducts } from './scrapers/googleScraper'
+import puppeteer from 'puppeteer'
 
-
-class App {
-    page: Page
-    url: string
-
-    constructor(page: Page, url: string) {
-        this.page = page
-        this.url = url
-    }
+const start = async () => {
+  return await puppeteer.launch({ headless: true })
 }
 
-const app = () => ({
-  start: async () => {
-    const { url } = googleConfig
-    const browser = await puppeteer.launch({ headless: true })
-    const page = await browser.newPage()
-    await page.goto(url)
-  },
-  getProducts: async () => {
-  },
-})
-
-export default app
