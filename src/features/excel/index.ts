794dce7fbed5fee461bdd004e877b07a4da853a5
Oguz <dev.oguz.kara@gmail.com>
2023-09-11 00:20:25 +0300

    function for saving analyzed product data to excel file

--- src/features/excel/index.ts
+++ src/features/excel/index.ts
@@ -1,7 +1,8 @@
 import ejs, { Cell, Row, Worksheet } from 'exceljs'
 import { ReadFile, ColumnData, ColumnNamesSet, RowData } from './types'
+import fs from 'fs'
 
-const getReadedFile: ReadFile = async (filepath) => {
+export const getReadedFile: ReadFile = async (filepath) => {
   // ! implement error handling
   // ! for sheet not founded
 
@@ -23,7 +24,10 @@ const extractColumnNames = (worksheet: Worksheet) => {
   return columnNames
 }
 
-const extractDataFromWorksheet = (worksheet: Worksheet, columnNames:ColumnNamesSet ) => {
+const extractDataFromWorksheet = (
+  worksheet: Worksheet,
+  columnNames: ColumnNamesSet,
+) => {
   const result: RowData[] = []
   worksheet.eachRow({ includeEmpty: false }, (row: Row) => {
     const rowData: RowData = {}
@@ -37,11 +41,11 @@ const extractDataFromWorksheet = (worksheet: Worksheet, columnNames:ColumnNamesS
   return result
 }
 
-const getColumnDataFromWorksheet: ColumnData = (worksheet) => {
+export const getColumnDataFromWorksheet: ColumnData = (worksheet) => {
   try {
     const columnNames = extractColumnNames(worksheet)
     const data = extractDataFromWorksheet(worksheet, columnNames)
-    
+
     return data
   } catch (error) {
     console.error('Error:', error)
@@ -49,7 +53,40 @@ const getColumnDataFromWorksheet: ColumnData = (worksheet) => {
   }
 }
 
-export default {
-  getReadedFile,
-  getColumnDataFromWorksheet,
+export const writeDataToXlsxFile = (data: any) => {
+  // Create a new workbook and add a worksheet
+  const workbook = new ejs.Workbook()
+  const worksheet = workbook.addWorksheet('Sheet 1')
+
+  // Define the headers
+  worksheet.columns = [
+    { header: 'barkod', key: 'A' },
+    { header: 'urun ismi', key: 'B' },
+    { header: 'marka', key: 'C' },
+    { header: 'kategori', key: 'D' },
+    { header: 'alis fiyati', key: 'E' },
+    { header: 'satis fiyati', key: 'F' },
+    { header: 'stok adedi', key: 'G' },
+    { header: 'stok turu', key: 'H' },
+    { header: 'onerilen satis fiyati', key: 'recommendedSalePrice' },
+    {
+      header: 'rakip ortalama satis fiyati',
+      key: 'averageCompatitorSalePrice',
+    },
+    // Add more headers as needed
+  ]
+
+  // Add the data to the worksheet
+  worksheet.addRows(data)
+
+  // Save the workbook to a file
+  const filePath = 'output.xlsx'
+  workbook.xlsx
+    .writeFile(filePath)
+    .then(() => {
+      console.log(`Excel file saved as ${filePath}`)
+    })
+    .catch((error) => {
+      console.error('Error saving Excel file:', error)
+    })
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { scraper } from '@features/scrapping'
-import fs from 'fs'
 import path from 'path'
-import excel from '@features/excel'
+import {
+  getReadedFile,
+  getColumnDataFromWorksheet,
+  writeDataToXlsxFile,
+} from '@features/excel'
 import { getSimilarProducts } from '@features/product-filter/lib/productFilter'
 import { analyzeProductListPrices } from '@features/analyze/lib/analyze'
 import { ProductWithRelateds } from '@features/product-filter/types'
@@ -15,21 +18,16 @@ const excelFilePath = path.resolve(
 )
 
 const start = async () => {
-  const workbook = await excel.getReadedFile(excelFilePath)
-  const data = excel.getColumnDataFromWorksheet(workbook)
+  const workbook = await getReadedFile(excelFilePath)
+  const data = getColumnDataFromWorksheet(workbook)
   const testData = data.slice(267, 273)
 
   const scrappedData = await scraper.google.shoppingScraper(testData)
   const similarProducts = scrappedData?.map((item) => getSimilarProducts(item))
-  const analyzedProduct = analyzeProductListPrices(similarProducts as ProductWithRelateds[])
-
-  fs.writeFile('result.txt', JSON.stringify(analyzedProduct), (err) => {
-    if (err) console.log(err)
-    else {
-      console.log('File written successfully\n')
-      console.log('The written has the following contents:')
-    }
-  })
+  const analyzedProducts = analyzeProductListPrices(
+    similarProducts as ProductWithRelateds[],
+  )
+  writeDataToXlsxFile(analyzedProducts)
 }
 
 start()
